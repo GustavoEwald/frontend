c376634f3d2582ab15bb880df93f8909eb3c0938
Gustavo Ewald <155274383+GustavoEwald@users.noreply.github.com>
2024-06-23 17:20:17 -0300

    Add exercicio typescript

---
+++ exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicar(num1: number, num2: number): number {
+  return num1*num2;
+}
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+// testes
+// console.log(multiplicar(29, 3));
+// console.log(saudacao('Pedro'));
